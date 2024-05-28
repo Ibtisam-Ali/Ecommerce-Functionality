7bf3e390f0ab6e605ef32a039a13616d115d59af
Ibtisam-Ali <alimemonpk17@gmail.com>
2024-05-28 18:31:58 +0500

    Ecommerce

---
+++ app.js
@@ -0,0 +1,132 @@
+const phones = [
+    {
+        Image:  src="./images/s20.jpg",
+        brand: 'Samsung',
+        model: 'S20',
+        ram: 8,
+        rom: 256,
+        camera: '20 megapixel',
+        price: 15000,
+       
+    },
+    {
+        Image:  src="./images/note 10.jpg",
+        brand: 'Xiomi',
+        model: 'note10',
+        ram: 4,
+        rom: 64,
+        camera: '10 megapixel',
+        price: 15000
+    },
+    {
+        Image:  src="./images/z10.jpg",
+        brand: 'Infinix',
+        model: 'z10',
+        ram: 2,
+        rom: 16,
+        camera: '5 megapixel',
+        price: 15000
+    },
+    {
+        Image:  src="./images/spark 10.jpg",
+        brand: 'Tecno',
+        model: 'spark10',
+        ram: 12,
+        rom: 512,
+        camera: '25 megapixel',
+        price: 15000
+    },
+    {
+        Image:  src="./images/14.avif",
+        brand: 'Iphone',
+        model: '14',
+        ram: 4,
+        rom: 1024,
+        camera: '30 megapixel',
+        price: 15000
+    },
+    {
+        Image:  src="./images/f11.jpg",
+        brand: 'Oppo',
+        model: 'F11',
+        ram: 8,
+        rom: 256,
+        camera: '20 megapixel',
+        price: 15000
+    },
+    {
+        Image:  src="./images/y20.jpg",
+        brand: 'Vivo',
+        model: 'y20',
+        ram: 4,
+        rom: 64,
+        camera: '8 megapixel',
+        price: 15000
+    },
+    {
+        Image:  src="./images/a52.jpg",
+        brand: 'Samsung',
+        model: 's50',
+        ram: 50,
+        rom: 1024,
+        camera: '60 megapixel',
+        price: 300000
+    },
+    {
+        Image: src="./images/s3.webp",
+        brand: 'Samsung',
+        model: 's8',
+        ram: 8,
+        rom: 256,
+        camera: '60 megapixel',
+        price: 70000
+    },
+
+];
+
+const arr = [];
+
+const div = document.querySelector('#div2')
+
+
+function  phoneRender() {
+    for(let i = 0; i < phones.length; i++){
+    
+        div.innerHTML += `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <img src="${phones[i].Image}">
+      <h5 class="card-title">${phones[i].brand+' '+phones[i].model}</h5>
+      <h6 class="card-subtitle mb-2 text-body-secondary">RAM: ${phones[i].ram}</h6>
+      <h6 class="card-subtitle mb-2 text-body-secondary">ROM: ${phones[i].rom}</h6>
+      <h6 class="card-subtitle mb-2 text-body-secondary">Camera: ${phones[i].camera}</h6>
+      <h6 class="card-subtitle mb-2 text-body-secondary">Price: ${phones[i].price}</h6>
+      
+      <button onclick="addToCart(${i})" 
+      class="btn btn-primary">Add to Cart</button>
+    </div>
+  </div>`
+    }
+    
+};
+phoneRender();
+
+
+function addToCart(index) {
+    // console.log('hello');
+    if (arr.includes(phones[index])) {
+        phones[index].quantity += 1;
+        
+    }else{
+        phones[index].quantity = 1;
+        arr.push(phones[index]);
+    }
+    console.log(arr);
+
+};
+
+function goToCart(){
+    console.log('cart');
+ localStorage.setItem('cartItems' , JSON.stringify(arr));
+    window.location = 'cart.html';
+}
+console.log(localStorage.setItem);
---
+++ cart.js
@@ -0,0 +1,99 @@
+console.log('cart items');
+
+const cartItems = JSON.parse(localStorage.getItem('cartItems'));
+// console.log(cartItems)
+
+const div = document.querySelector('div');
+let totalAmount = document.querySelector('#totalAmount')
+
+function renderItems (){
+    if(cartItems != null && cartItems.length > 0){
+        for(let i = 0; i < cartItems.length; i++){
+            console.log(cartItems[i]);
+            div.innerHTML += `<div class="card" style="width: 18rem;">
+            <div class="card-body">
+             <img src="${cartItems[i].Image}">
+              <h5 class="card-title">${cartItems[i].brand+' '+cartItems[i].model}</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">RAM: ${cartItems[i].ram}</h6>
+              <h6 class="card-subtitle mb-2 text-body-secondary">ROM: ${cartItems[i].rom}</h6>
+              <h6 class="card-subtitle mb-2 text-body-secondary">Camera: ${cartItems[i].camera}</h6>
+              <h6 class="card-subtitle mb-2 text-body-secondary">Price: ${cartItems[i].price}</h6>
+              <p class="card-text mt-2">Quantity : <button class='btn btn-danger' onclick='lessQuantity(${i})'>-</button>${cartItems[i].quantity} <button class='btn btn-danger' onclick='addQuantity(${i})'>+</button></p><br />
+              
+              <button onclick="reset(${i})" 
+              class="btn btn-primary">Delete</button>
+
+        
+            </div>
+          </div>`
+        }
+    }else{
+        console.log('cart item empty ha maalik'
+
+
+
+
+        
+
+
+        )
+        div.innerHTML = `
+        <h3 class="text-center">No Item Found...</h3>
+        `
+    }
+}
+
+renderItems()
+
+
+
+
+function reset(i) {
+     cartItems.splice(i ,1)
+     localStorage.setItem('cartItems', JSON.stringify(cartItems))
+     location.reload()
+     
+   
+};
+
+
+function updatePrice() {
+  let totalPrice = 0;
+  for (let i = 0; i < cartItems.length; i++)
+    totalPrice = totalPrice +(cartItems[i].price * cartItems[i].quantity)
+   totalAmount.innerHTML = `Total Amount ${totalPrice}`
+};
+updatePrice()
+
+
+function addQuantity(i) {
+  cartItems[i].quantity += 1
+  localStorage.setItem('cartItems', JSON.stringify(cartItems))
+  location.reload()
+  updatePrice()
+}
+
+
+
+function lessQuantity(i) {
+  if (cartItems[i].quantity === 1) {
+      cartItems.splice(i, 1)
+      localStorage.setItem('cartItems', JSON.stringify(cartItems))
+      location.reload()
+  } else {
+      cartItems[i].quantity -= 1
+      localStorage.setItem('cartItems', JSON.stringify(cartItems))
+      location.reload()
+      updatePrice()
+  }
+}
+
+
+
+
+// localStorage.setItem('kis naam sa krwana ha' , 'value stringify wali');
+
+// localStorage.getItem('kis item sa save krwaya ha');
+
+// JSON.stringify()
+// JSON.parse()
